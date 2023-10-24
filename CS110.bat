f9582dd65f9787a9a4f69dc673a1abd0f6ede8dd
Thaninki <145518618+Thaninki@users.noreply.github.com>
2023-10-25 02:54:42 +0530

    first commit

---
+++ CS110.bat
@@ -0,0 +1,9 @@
+lease enter the following information:
+
+First name: sunanda
+Last name: Thaninki
+Email address: salysynanda
+Phone number: (2086071612)
+Job title:bank account
+ID Number: 219518917
+----------------------------------------
