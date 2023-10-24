ec379b320a8b31ce4d9f7a8e7768fed3bc4d14bb
Thaninki <145518618+Thaninki@users.noreply.github.com>
2023-10-25 02:59:35 +0530

    cleanup

--- CS110.bat
+++
@@ -1,9 +0,0 @@
-lease enter the following information:
-
-First name: sunanda
-Last name: Thaninki
-Email address: salysynanda
-Phone number: (2086071612)
-Job title:bank account
-ID Number: 219518917
-----------------------------------------
